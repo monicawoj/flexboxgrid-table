0e9a05f7d5b1140e46821c62585d0f623f6c5c60
Monica Wojciechowski <monica.wojciechowski@gmail.com>
2018-08-07 23:53:30 +0200

    complete js event listeners

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,34 +1,63 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
    document.querySelectorAll('.table__column--delete').forEach(btn => {
+       let myRow = btn.parentNode;
+
+       btn.addEventListener('mouseover', () => {
+           myRow.classList.add('hover');
+       });
+
+       btn.addEventListener('mouseout', () => {
+           myRow.classList.remove('hover');
+       });
 
         btn.addEventListener('click', () => {
-            console.log(btn.parentNode);
-
-            let myRow = btn.parentNode;
             let newRow=document.createElement("div");
-            newRow.innerHTML=`<div class="table__row">
-            <div class="table__column _8">
-                <p>Test</p>
-            </div>
-            <div class="table__column _6">
-                <p>12</p>
-            </div>
-            <div class="table__column _6">
-                <p>Gemini</p>
-            </div>
-            <div class="table__column table__column--delete _4">
-                <p class="hidden">DELETE</p>
-                <div class="delete-icon"></div>
-            </div>
-        </div>`;
-            if(myRow.nextSibling){
+            if([...myRow.classList].indexOf('table__row--last') < 0){
+                newRow.innerHTML=`<div class="confirm">
+                 <div class="centered">
+                    <div class="table__column _18 confirm__message">
+                        <p>Are you sure you want to remove this item?</p>
+                    </div>
+                    <div class="table__column confirm__button confirm__button--no _3">
+                        <p>No</p>
+                    </div>
+                    <div class="table__column confirm__button confirm__button--yes _3">
+                        <p>Yes</p>
+                    </div>
+                  </div>
+                </div>`;
                 myRow.parentNode.insertBefore(newRow,myRow.nextSibling);
             }else{
-                myRow.parentNode.appendChild(newRow);
+                newRow.innerHTML=`<div class="confirm confirm--last">
+                 <div class="centered">
+                    <div class="table__column _18 confirm__message">
+                        <p>Are you sure you want to remove this item?</p>
+                    </div>
+                    <div class="table__column confirm__button confirm__button--no _3">
+                        <p>No</p>
+                    </div>
+                    <div class="table__column confirm__button confirm__button--yes _3">
+                        <p>Yes</p>
+                    </div>
+                  </div>
+                </div>`;
+                myRow.parentNode.insertBefore(newRow,myRow);
             }
 
+            myRow.classList.add('click');
 
+            document.querySelector('.confirm__button--yes').addEventListener('click', () => {
+                console.log('clicked yes');
+                myRow.parentNode.removeChild(myRow);
+                newRow.parentNode.removeChild(newRow);
+            });
+
+            document.querySelector('.confirm__button--no').addEventListener('click', () => {
+                console.log('clicked no');
+                newRow.parentNode.removeChild(newRow);
+                myRow.classList.remove('click');
+            });
         });
     });
 
